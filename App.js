235141cfc81f4ad99a8bebfed2e8340a973f29d6
Eduardo Santos <97476026+EduardoASCS@users.noreply.github.com>
2022-05-02 15:49:22 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-
 import React, { useState} from 'react';
 import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View, } from 'react-native';
 import Button from './src/Button';
